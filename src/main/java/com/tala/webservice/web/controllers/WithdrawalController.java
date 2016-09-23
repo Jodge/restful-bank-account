b63658f739ca1b8f7af15aed9d29f176740df287
George Otieno <georgeorti@gmail.com>
2016-09-23 20:48:20 +0300

    resolved conflicts

---
+++ src/main/java/com/tala/webservice/web/controllers/WithdrawalController.java
@@ -0,0 +1,10 @@
+package com.tala.webservice.web.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/withdrawal")
+public class WithdrawalController extends BaseController {
+
+}
